86dbe91a09eb598dcc5087e8012164bbdcfedfd2
Myoth <louisonmartin@gmail.com>
2020-03-05 15:56:51 +0100

    added thermal monitor

--- script xbps.bash
+++ script xbps.bash
@@ -12,15 +12,24 @@ xbps-install -u xbps
 xbps-install -Su
 
 #installing KDE-plasma and its base apps
-xbps-install -Su kde5 kde5-baseapps
+xbps-install -Su kde5 && kde5-baseapps
+
+#removing XFCE4 and its software
+xbps-remove -oR xfce4
 
 #installing various software
-xbps-install telegram-desktop mpd cantata libreoffice-kde flameshot anki acpi 
+xbps-install telegram-desktop && mpd && cantata && libreoffice-kde && flameshot && anki && acpi && neofetch && git
 
 #installing spotify through the non-free repo
 xbps-install -S void-repo-nonfree
 xbps-install -Su
 xbps-install -Su spotify
 
-#removing XFCE4 and its software
-xbps-remove -oR xfce4
+#adding the thermal monitor
+git clone --depth=1 https://github.com/kotelnik/plasma-applet-thermal-monitor
+cd plasma-applet-thermal-monitor/
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+sudo make install
+
